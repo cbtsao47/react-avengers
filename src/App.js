f605c68d7338c771d964ef640d9ec3ac22e8c1d3
cbtsao47 <cbtsao47@gmail.com>
2018-11-11 22:11:28 -0600

    added list of avengers

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { list } from 'postcss';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      avengers:['Iron-man','Black-widow','Hawkeye',"Hulk",'Thor']
+    };
+  }
+
+
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
           <a
             className="App-link"
             href="https://reactjs.org"
@@ -19,6 +26,9 @@ class App extends Component {
           >
             Avengers: by Ben Tsao
           </a>
+        <ul>
+          {this.state.avengers.map((avenger, i) => <li key={i}>{avenger}</li> )}
+        </ul>
         </header>
       </div>
     );
