02f39ee4787c2a46ac54c995d4b5532e30370b82
cbtsao47 <cbtsao47@gmail.com>
2018-11-11 21:52:59 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Avengers: by Ben Tsao
           </a>
         </header>
       </div>
